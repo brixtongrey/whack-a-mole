7f540ae3c9ea0b2e8b9c4ff82ff228370f8c2a6f
Brixtongrey <brixtonaruda@gmail.com>
2025-09-14 15:19:42 -0600

    feat: GameProvider function

--- src/game/GameContext.jsx
+++ src/game/GameContext.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const NUM_HOLES = 9;
 const TIME_LIMIT = 10;
@@ -6,6 +6,19 @@ const TIME_LIMIT = 10;
 const GameContext = createContext();
 
 export function GameProvider({ children }) {
+
+    const [holes, setHoles] = useState();
+    const [score, setScore] = useState(0);
+    const [time, setTime] = useState(TIME_LIMIT);
+    const [playing, setPlaying] = useState(false);
+
+    // keeps track of the game time, it ends the round by calling stop
+    useEffect(() => {
+        if (time <=0 && playing) {
+            //stop
+        }
+    }, [time, [playing]]);
+
     return <GameContext.Provider value={5}>{children}</GameContext.Provider>
 }
 
